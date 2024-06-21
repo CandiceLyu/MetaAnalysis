72e5d8960b3c0cde736ef8e987efabf689e0996c
Dr Nathan Green <n8thangreen@yahoo.co.uk>
2024-06-21 11:45:58 +0100

    small edits

--- docs/base_case_analysis.R
+++ docs/base_case_analysis.R
@@ -11,20 +11,21 @@ library(R2OpenBUGS)
 library(mcmcplots)
 library(bayesplot)
 library(ggplot2)
+library(here)
 
 # Model set-up
 # filein <- "BUGS/Base-Case Model_BUGS_code.txt"
-bc_model <- "C:/Users/zcahtly/Desktop/Base_Case_Model_BUGS_code.txt"
-
+bc_model <- here("BUGS/Base_Case_Model_BUGS_code.txt")
 
 # Data
-events_data <- read.csv("covid_vaccine_data_r.csv")
-total_vaccination_data <- read.csv("covid_vaccine_data_n.csv")
+events_data <- read.csv(here("raw_data/covid_vaccine_data_r.csv"))
+total_vaccination_data <- read.csv(here("raw_data/covid_vaccine_data_n.csv"))
+
 # Data for observed no. of events and total vaccinated people
 # excluding the third column indicating the study id
 r <- as.matrix(events_data[, -3])
 n <- as.matrix(total_vaccination_data[, -3])
-bc_data <- list(r = r, n = n)
+bc_data <- tibble::lst(r, n)
 
 
 # Initial values for chains
@@ -35,11 +36,8 @@ bc_inits <- list(
   list(d = c(2, 4, 2, 4, 2, NA, NA, NA, NA))
 )
 
-
-
 # Parameters to monitor
 bc_params <- c("d", "rr", "totresdev")
-
 
 n.iter <- 20000
 n.burnin <- 1000
@@ -50,14 +48,15 @@ n.thin <- floor((n.iter - n.burnin) / 500)
 base_case <- bugs(
   data = bc_data, 
   inits = bc_inits, 
-  parameters.to.save = bc_params, 
+  parameters.to.save = bc_params,
   model.file = bc_model,
   n.chains = 4, 
   n.iter = 20000,
   n.burnin = 1000,
   n.thin = 1,
-  DIC = TRUE,
-  debug = TRUE)
+  DIC = TRUE
+  # debug = TRUE
+  )
 
 
 print(base_case)
