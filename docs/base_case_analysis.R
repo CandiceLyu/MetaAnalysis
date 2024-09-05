966077315c6d1dbc7c49b2d0becde5d566581142
Candice Lyu <466837094@qq.com>
2024-09-05 22:41:27 +0800

    Update base_case_analysis.R

--- docs/base_case_analysis.R
+++ docs/base_case_analysis.R
@@ -1,69 +1,60 @@
-# BUGS Base-Case Model running script
+# Base-Case BUGS Model running script
 
 # Library relevant packages
-library(R2jags)
-library(dplyr)
-library(reshape2)
-library(mcmc)
-library(coda)
-library(lattice)
-library(R2OpenBUGS)
-library(mcmcplots)
-library(bayesplot)
-library(ggplot2)
-library(here)
+library(R2jags)        # For running JAGS models
+library(dplyr)         # For data manipulation
+library(mcmc)          # For MCMC diagnostics
+library(coda)          # For MCMC output analysis
+library(lattice)       # For data visualisation
+library(R2OpenBUGS)    # Interface for OpenBUGS
+library(mcmcplots)     # For MCMC diagnostic plots
+library(bayesplot)     # For Bayesian data visualisation
+library(ggplot2)       # For creating plots
 
-# Model set-up
-# filein <- "BUGS/Base-Case Model_BUGS_code.txt"
-bc_model <- here("BUGS/Base_Case_Model_BUGS_code.txt")
+# Specify the model file path
+bc_model <- "C:/Users/lvtianying/Desktop/base_case_code.txt"
 
-# Data
-events_data <- read.csv(here("raw_data/covid_vaccine_data_r.csv"))
-total_vaccination_data <- read.csv(here("raw_data/covid_vaccine_data_n.csv"))
+# Load data: observed outcomes and total number of vaccinated individuals
+outcomes <- readxl::read_xlsx("node_r.xlsx")
+total_vaccination <- readxl::read_xlsx("node_n.xlsx")
+# Prepare data for the model (excluding the study ID column)
+bc_r <- as.matrix(outcomes[, -3])
+bc_n <- as.matrix(total_vaccination[, -3])
+bc_data <- list(r = bc_r, n = bc_n)
 
-# Data for observed no. of events and total vaccinated people
-# excluding the third column indicating the study id
-r <- as.matrix(events_data[, -3])
-n <- as.matrix(total_vaccination_data[, -3])
-bc_data <- tibble::lst(r, n)
-
-
-# Initial values for chains
+# Define initial values for four MCMC chains
 bc_inits <- list(
-  list(d = c(0, 0, 0, 0, 0, NA, NA, NA, NA)),
-  list(d = c(-1, -3, -1, -3, -1, NA, NA, NA, NA)),
-  list(d = c(1, -3, 1, -3, 1, NA, NA, NA, NA)),
-  list(d = c(2, 4, 2, 4, 2, NA, NA, NA, NA))
+  list(d = rep(0, 9)),
+  list(d = rep(0.7, 9)),
+  list(d = rep(-3, 9)),
+  list(d = c(2, 4, 2, 4, 2, 2, 4, 2, 4))
 )
 
-# Parameters to monitor
-bc_params <- c("d", "rr", "totresdev")
+# Parameters to monitor during the model run
+bc_params <- c("d", "rr", "totresdev", "sd")
 
-n.iter <- 20000
-n.burnin <- 1000
-n.thin <- floor((n.iter - n.burnin) / 500)
-
-
-# Run WinBUGS using R2WinBUGS
-base_case <- bugs(
+# Run the model using OpenBUGS
+base_case <- R2OpenBUGS::bugs(
   data = bc_data, 
   inits = bc_inits, 
-  parameters.to.save = bc_params,
+  parameters.to.save = bc_params, 
   model.file = bc_model,
   n.chains = 4, 
   n.iter = 20000,
   n.burnin = 1000,
-  n.thin = 1,
-  DIC = TRUE
-  # debug = TRUE
-  )
+  n.thin = 1, 
+  DIC = TRUE                         # Calculate DIC for model comparison
+)
 
-
-print(base_case)
+# Output the model summary
 base_case$summary
-
-
-R2WinBUGS::attach.bugs(base_case)
-
-mcmc_intervals
-plot(base_case)
+# Create MCMC diagnostic plots for the relative risks
+mcmcplots::mcmcplot(base_case, regex = "rr")
+# Convert MCMC samples to 'mcmc' object for diagnostics
+mcmc_samples <- as.mcmc(base_case)
+# Plot traceplots to assess chain convergence
+coda::traceplot(mcmc_samples)
+# Calculate and display the Gelman-Rubin diagnostic for convergence
+gelman.diag(mcmc_samples)
+# Generate a Gelman-Rubin plot for visual inspection of convergence
+gelman.plot(mcmc_samples)
